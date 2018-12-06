f7c47aa53777402014f328466a6dba9a2f9e0464
Stephen Grosjean <stephen-gm@hotmail.com>
2018-12-06 14:33:06 +0100

    Added UI

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
     [SerializeField] private int health = 6;
@@ -9,6 +10,7 @@ public class PlayerHealth : MonoBehaviour {
     [SerializeField] private float minimumPosition;
     [SerializeField] private float destroyPosition;
     [SerializeField] private float deathForceFall, deathForceLife;
+    [SerializeField] private Image[] healthSprites;
 
     private bool canRemoveLife = true;
     private const float lifeTime = 0.1f;
@@ -44,23 +46,67 @@ public class PlayerHealth : MonoBehaviour {
         }
     }
 
+    //Function to remove life
     public void RemoveLife(int lifeToRemove){
         if (canRemoveLife) {
             StartCoroutine("LifeTimer");
             health -= lifeToRemove;
+            UpdateHealthUI();
         }
     }
 
+    //Function to add life
     public void AddLife(int lifeToAdd){
         health += lifeToAdd;
     }
 
+
+    private void UpdateHealthUI()
+    {
+        switch (health)
+        {
+            case 6:
+                healthSprites[5].enabled = true;
+                break;
+
+            case 5:
+                healthSprites[5].enabled = false;
+                healthSprites[4].enabled = true;
+                break;
+
+            case 4:
+                healthSprites[4].enabled = false;
+                healthSprites[3].enabled = true;
+                break;
+
+            case 3:
+                healthSprites[3].enabled = false;
+                healthSprites[2].enabled = true;
+                break;
+
+            case 2:
+                healthSprites[2].enabled = false;
+                healthSprites[1].enabled = true;
+                break;
+
+            case 1:
+                healthSprites[1].enabled = false;
+                healthSprites[0].enabled = true;
+                break;
+            case 0:
+                healthSprites[0].enabled = false;
+                break;
+        }
+    }
+
+    //Timer to remove life
     IEnumerator LifeTimer(){
         canRemoveLife = false;
         yield return new WaitForSeconds(lifeTime);
         canRemoveLife = true;
     }
 
+    //Death sequence
     IEnumerator DeathSequence(string Type){
         playerMovementScript.enabled = false;
         colliderScript.isTrigger = true;
