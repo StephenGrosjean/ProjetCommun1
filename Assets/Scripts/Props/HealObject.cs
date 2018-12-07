fa3538ee3bae7c9378093c8ad3507f01f4149fc7
Stephen Grosjean <stephen-gm@hotmail.com>
2018-12-07 15:09:37 +0100

    Added HealObject

---
+++ Assets/Scripts/Props/HealObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealObject : MonoBehaviour {
+    [SerializeField] private int healPoints;
+    private GameObject gameManager;
+    private GameManager gameManagerScript;
+	void Start () {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameManagerScript.AddLife(healPoints);
+            Destroy(gameObject);
+        }
+    }
+}
